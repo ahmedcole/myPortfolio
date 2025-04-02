ac2c4e8b0d0ee29a7b814c83b37e1757a8872cef
ahmed ajitena <pacsizzo07@gmail.com>
2025-04-02 03:24:01 +0100

    remain responsive for the home page and i have not deleted the existing files

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+window.addEventListener("beforeUnload", () => {
+  localStorage.setItem("scrollPosition", window.scrollY);
+});
+
+window.addEventListener("load", () => {
+  let scrollPosition = localStorage.getItem("scrollPosition");
+  if (scrollPosition) {
+    window.scrollTo(0, scrollPosition);
+  }
+});
+
 document.querySelectorAll(".nav-link").forEach((link) => {
   link.addEventListener("mouseenter", () => {
     document.querySelector("#active").classList.remove("active");
